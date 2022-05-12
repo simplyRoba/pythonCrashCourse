f83fa2219d6d5712059230a24661f51f7fdda6ae
Axel <git@simplyroba.de>
2022-05-12 18:21:25 +0200

    added exercise of chapter 2

--- chapter_02/exercise.py
+++ chapter_02/exercise.py
@@ -1,3 +1,4 @@
+# Strings
 name = "Peter"
 print(f"Hello {name}, would you like to learn some Python today?")
 
@@ -7,10 +8,18 @@ print(name.title())
 
 famous_person_first_name = "albert"
 famous_person_last_name = "einstein"
-
 quote = "   A person who never made a mistake never tried anything new.   "
-
 print(f"\t{famous_person_first_name.title()} {famous_person_last_name.title()} once said, \n\t\"{quote.rstrip().lstrip()}\"")
+
+
+# Numbers
+print(2+6)
+print(9-1)
+print(64/8)
+print(2*4)
+
+favorite_number = 13
+print(favorite_number)
 
 
 
