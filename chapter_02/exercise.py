36d44083d4c49ff8673b42b0eb634b3db8f51c92
Axel <git@simplyroba.de>
2022-05-12 17:58:45 +0200

    added exercise of chapter 2

---
+++ chapter_02/exercise.py
@@ -0,0 +1,16 @@
+name = "Peter"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+famous_person_first_name = "albert"
+famous_person_last_name = "einstein"
+
+quote = "   A person who never made a mistake never tried anything new.   "
+
+print(f"\t{famous_person_first_name.title()} {famous_person_last_name.title()} once said, \n\t\"{quote.rstrip().lstrip()}\"")
+
+
+
