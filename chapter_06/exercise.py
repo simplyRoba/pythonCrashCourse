3e13e11881f255fc5884bce0ef28411dd5ab459b
Axel <git@simplyroba.de>
2022-05-14 14:47:54 +0200

    added exercise of chapter 6

---
+++ chapter_06/exercise.py
@@ -0,0 +1,28 @@
+# 6-1
+person = {
+    'first_name': 'Peter',
+    'last_name': 'Schmitt',
+    'age': 45,
+    'city': 'Chicago'
+}
+for k, v in person.items():
+    print(f"{k}: {v}")
+
+# 6-2
+favorite_numbers = {
+    'Sarah': 29,
+    'Peter': 45,
+    'Monica': 23,
+    'Sheldon': 42,
+    'Barney': 69
+}
+for name, number in favorite_numbers.items():
+    print(f"{name}'s favorite number is {number}")
+
+# 6-3
+print("Hell! No!")
+
+
+
+
+
